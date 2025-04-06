682af17a33841a874aed86f5e9c29ad50b4fea04
mike <mike@lamoureux.me>
2025-04-06 12:09:47 -0400

    fix: Update default API URL and API key in plugin configuration

--- Auto Lights.indigoPlugin/Contents/Server Plugin/plugin.py
+++ Auto Lights.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -38,8 +38,10 @@ class Plugin(indigo.PluginBase):
         self.debug: bool = True
         self._agent = None
 
-        os.environ["INDIGO_API_URL"] = plugin_prefs.get("indigo_api_url", "xxx")
-        os.environ["API_KEY"] = plugin_prefs.get("api_key", "xxx")
+        os.environ["INDIGO_API_URL"] = plugin_prefs.get(
+            "indigo_api_url", "https://myreflector.indigodomo.net"
+        )
+        os.environ["API_KEY"] = plugin_prefs.get("api_key", "xxxxx-xxxxx-xxxxx-xxxxx")
         self._web_config_bind_ip = plugin_prefs.get("web_config_bind_ip", "127.0.0.1")
         self._web_config_bind_port = plugin_prefs.get("web_config_bind_port", "9000")
 
